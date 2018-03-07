052e5831995990d2312096d742a0e8e8f0a8d687
Malcolm Mielle <malcolm.mielle@gmail.com>
2018-03-07 17:30:45 +0100

    updated gt_evaluation file

--- src/evaluation_gt.cpp
+++ src/evaluation_gt.cpp
@@ -105,89 +105,92 @@ void draw(AASS::maoris::GraphZone& gp_real, AASS::maoris::GraphZone& gp_model, c
 
 
 
-void makeGraph(cv::Mat& slam, AASS::maoris::GraphZone& graph_slam, double& time){
-		
-	double begin_process, end_process, decompose_time;
-	std::cout << "/************ FUZZY OPENING*************/ \n";
-	AASS::maoris::FuzzyOpening fuzzy_slam;
-	fuzzy_slam.fast(false);
-	
-	cv::Mat out_slam;
-	
-	begin_process = getTime();	
-	fuzzy_slam.fuzzyOpening(slam, out_slam, 500);
-	end_process = getTime();	decompose_time = end_process - begin_process;
-	time = decompose_time;
-	
-	std::cout << "Fuzzy opening time: " << time << std::endl;
-	
-	out_slam.convertTo(out_slam, CV_8U);
-		
-	std::cout << "/************ REDUCING THE SPACE OF VALUES *****************/\n";
-	cv::Mat out_tmp_slam;
-	AASS::maoris::ZoneExtractor zone_maker;
-	zone_maker.addValueToIgnore(0);
-	
-	begin_process = getTime();
-	AASS::maoris::reduceZone(out_slam, out_tmp_slam, 5);
-	zone_maker.extract(out_tmp_slam);
-	end_process = getTime();	decompose_time = end_process - begin_process;
-	time = time + decompose_time;
-	
-	std::cout << "Zone reducing: " << decompose_time << std::endl;
-		
-	std::cout << "/*********** MAKING AND TRIMMING THE GRAPH ***************/\n";
-	
-	int size_to_remove2 = 10;
-	
-	begin_process = getTime();
-	graph_slam = zone_maker.getGraph();
-// 	graph_slam.setThreshold(0.25);
-	
-	graph_slam.setThreshold(0.45);
+void process(const std::string& file, bool write = false){
+
+	AASS::maoris::GraphZone graph_slam;
+
+	graph_slam.setThreshold(0.30);
 	graph_slam.setMargin(0.1);
 	graph_slam.setThresholdFusionRipples(40);
-	graph_slam.setThresholdFusionDoors(15);
-	
-	std::cout << " graph_src.getThresholdFusionDoors(); " << graph_slam.getThresholdFusionDoors() << std::endl;
-	std::cout << " graph_src.getThreshold(); " << graph_slam.getT() << std::endl;
-	std::cout << " graph_src.getThresholdmargin(); " << graph_slam.getMargin() << std::endl;
-	std::cout << " graph_src.getThresholdFusionRipples(); " << graph_slam.getThresholdFusionRipples() << std::endl;
-	
-// 	graph_slam.removeVertexValue(0);	
-	graph_slam.removeVertexUnderSize(size_to_remove2, true);
+	graph_slam.setThresholdFusionDoors(20);
 
-	graph_slam.useCvMat(true);
-// 	graph_slam.updatePCA();
-	graph_slam.updateContours();
-	graph_slam.removeRiplesv3();
-	
-	end_process = getTime();	decompose_time = end_process - begin_process;
-	time = time + decompose_time;
-	std::cout << "Ripples: " << decompose_time << std::endl;
-	
-	begin_process = getTime();
-// 	graph_slam.updatePCA();
-	graph_slam.updateContours();
-	
-	//Watershed Algorithm
-	graph_slam.watershed();
-	
-	graph_slam.removeDoors();
-	
-	int size_to_remove = 100;
-	graph_slam.removeVertexUnderSize(size_to_remove, true);
-	graph_slam.removeLonelyVertices();
-	end_process = getTime();	decompose_time = end_process - begin_process;
-	time = time + decompose_time;
-	
-	std::cout << "watershed: " << decompose_time << std::endl;
-	
-	if(graph_slam.lonelyVertices())
-		throw std::runtime_error("Fuck you lonelyness");	
-	
+	cv::Mat slam_in = cv::imread(file, CV_LOAD_IMAGE_GRAYSCALE);
+	assert(CV_LOAD_IMAGE_GRAYSCALE == 0);
+	cv::Mat slam = slam_in > 250;
+
+	cv::resize(slam, slam, cv::Size(slam.cols / 4, slam.rows / 4));
+
+// 	cv::threshold(slam, slam, 20, 255, cv::THRESH_BINARY);
+// 	cv::threshold(slam, slam, 20, 255, cv::THRESH_BINARY_INV);
+// 	cv::imshow("map in", slam);
+// 	cv::waitKey(0);
+
+
+	double time = 0;
+// 	makeGraph(slam, graph_slam, time);
+	AASS::maoris::Segmentor segmenteur;
+	time = segmenteur.segmentImage(slam, graph_slam);
+	cv::Mat segmented_map = segmenteur.getSegmentedMap();
+
+	std::cout << "Total time: " << time << std::endl;
+
+	/********** PCA of all zones in Graph and removing the ripples **********/
+// 	graph_slam.update();
+
+
+	cv::Mat slam1 = cv::imread(file, CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat graphmat = cv::Mat::zeros(slam1.size(), CV_8U);
+	graph_slam.drawEvaluation(graphmat);
+
+// 	cv::Mat graphmat_vis = cv::Mat::zeros(slam1.size(), CV_8U);
+//     graph_slam.draw(graphmat_vis);
+// 	cv::resize(graphmat, graphmat, cv::Size(graphmat.cols * 4, graphmat.rows * 4));
+ 	cv::imshow("GRAPH Visible", graphmat);
+// //
+// // 	std::cout << "Zones " << graph_slam.getNumVertices() << std::endl;
+//
+//
+// // 	cv::Mat partial = cv::Mat::zeros(slam1.size(), CV_8U);
+// //     graph_slam.drawPartial(partial);
+//
+// 	cv::Mat img_hist_equalized;
+// 	cv::equalizeHist(graphmat, img_hist_equalized);
+// 	cv::resize(graphmat, graphmat, cv::Size(graphmat.cols * 2, graphmat.rows * 2));
+// 	cv::imshow("GRAPH", img_hist_equalized);
+// 	cv::waitKey(0);
+//
+// 	std::cout << graphmat << std::endl;
+// 	exit(0);
+
+
+// 	std::cout << "Size of graph" << graph_slam.getNumVertices() << std::endl;
+
+
+	//Reading the GT
+
+// 	std::cout << " GT " << full_path_GT << std::endl;
+//
+// 	cv::imshow("GT raw", image_GT);
+ 	cv::waitKey(0);
+
+	cv::Mat graphmat_straight = AASS::maoris::segment_Ground_Truth(segmented_map);
+
+	if(write == true){
+		std::vector<int> param;
+		param.push_back(CV_IMWRITE_PNG_COMPRESSION);
+		param.push_back(9);
+		boost::filesystem::path p(file);
+		std::string name = p.filename().stem().string();
+		cv::imwrite(name + "_straighten.png", graphmat_straight, param);
+		cv::imwrite(name + ".png", graphmat, param);
+	}
+
+// 	cv::Mat img_hist_equalizedgt;
+// 	cv::equalizeHist(GT_segmentation, img_hist_equalizedgt);
+// 	cv::imshow("GT", img_hist_equalizedgt);
+// 	cv::waitKey(0);
+
 }
-
 
 BOOST_AUTO_TEST_CASE(trying)
 {
@@ -196,109 +199,9 @@ BOOST_AUTO_TEST_CASE(trying)
 	char** argv = boost::unit_test::framework::master_test_suite().argv;
 		
 	std::string file = argv[1];
-	std::string full_path_GT = argv[2];
 // 	std::string file = "../../Test/Thermal/cold.jpg";
-	AASS::maoris::GraphZone graph_slam;
-	graph_slam.setThreshold(0.25);
-	graph_slam.setMargin(0.1);
-	graph_slam.setThresholdFusionRipples(40);
-	graph_slam.setThresholdFusionDoors(40);
-	
-	cv::Mat slam_in = cv::imread(file, CV_LOAD_IMAGE_GRAYSCALE);
-	assert(CV_LOAD_IMAGE_GRAYSCALE == 0);
-	cv::Mat slam = slam_in > 250;
-	
-	
-	
-// 	cv::threshold(slam, slam, 20, 255, cv::THRESH_BINARY);
-// 	cv::threshold(slam, slam, 20, 255, cv::THRESH_BINARY_INV);
-// 	cv::imshow("map in", slam);
-// 	cv::waitKey(0);
-	
-	AASS::maoris::Segmentor segmenteur;
-	
-	double time = 0;
-// 	makeGraph(slam, graph_slam, time);
-	
-	time = segmenteur.segmentImage(slam, graph_slam);
-	
-	cv::Mat segmented_map = segmenteur.getSegmentedMap();
-	
-	cv::imshow("straight segmented", segmented_map);
-	
-	std::cout << "Total time: " << time << std::endl;
-			
-	/********** PCA of all zones in Graph and removing the ripples **********/
-	graph_slam.updateContours();
 
-	
-	cv::Mat slam1 = cv::imread(file, CV_LOAD_IMAGE_GRAYSCALE);
-   cv::Mat graphmat = cv::Mat::zeros(slam1.size(), CV_8U);
-   graph_slam.drawEvaluation(graphmat);
-	
-// 	cv::Mat partial = cv::Mat::zeros(slam1.size(), CV_8U);
-//     graph_slam.drawPartial(partial);
-	
-	cv::Mat img_hist_equalized;
-	cv::equalizeHist(graphmat, img_hist_equalized);
-// 	cv::resize(graphmat, graphmat, cv::Size(graphmat.cols * 2, graphmat.rows * 2));
-	cv::imshow("GRAPH", img_hist_equalized);
-	
-// 	cv::Mat minus = graphmat - slam;
-// 	cv::Mat minus2 = slam - graphmat;
-// 	cv::imshow("black", minus);
-// 	cv::imshow("alos black", minus2);
-// 	cv::waitKey(0);
-	
-	cv::Mat graphmat_vis = cv::Mat::zeros(slam1.size(), CV_8U);
-	graph_slam.draw(graphmat_vis);
-// 	cv::resize(graphmat, graphmat, cv::Size(graphmat.cols * 2, graphmat.rows * 2));
-	cv::imshow("GRAPH Visible", graphmat_vis);
-	
-	cv::Mat graphmat_c = cv::Mat::zeros(slam1.size(), CV_8U);
-	graph_slam.drawContours(graphmat_c);
-	cv::imshow("contours", graphmat_c);
-	
-	std::cout << "Size of graph" << graph_slam.getNumVertices() << std::endl;
-   
-	
-	//Reading the GT
-	
-	cv::Mat image_GT = cv::imread(full_path_GT,0);
-// 	cv::imshow("GT raw", image_GT);
-	cv::waitKey(0);
-	
-	cv::Mat GT_segmentation = AASS::maoris::segment_Ground_Truth(image_GT);
-	cv::Mat graphmat_straight = AASS::maoris::segment_Ground_Truth(segmented_map);
-// 	cv::Mat graphmat = segment_Ground_Truth(slam);
-	
-	cv::Mat img_hist_equalizedgt;
-	cv::equalizeHist(GT_segmentation, img_hist_equalizedgt);
-	cv::imshow("GT", img_hist_equalizedgt);
-	
-	cv::Mat img_hist_equalizedseg;
-	cv::equalizeHist(graphmat_straight, img_hist_equalizedseg);
-	cv::imshow("seg eq", img_hist_equalizedseg);
-	
-	cv::waitKey(0);
-	
-// 	compareAndExport(graphmat, GT_segmentation, time, file);
-	AASS::maoris::Evaluation eval;
-	eval.compare(graphmat_straight, GT_segmentation, time, file);
-	eval.exportAll("test.dat");
-	
-	std::vector<int> param;
-	param.push_back(CV_IMWRITE_PNG_COMPRESSION);
-	param.push_back(9);
-	boost::filesystem::path p(file);
-	std::string name = p.filename().stem().string();
-	cv::imwrite(name + "_straighten.png", graphmat_straight, param);
-	cv::imwrite(name + ".png", graphmat, param);
-	
-	cv::imshow(name + "_straighten.png", graphmat_straight);
-	cv::imshow(name + ".png", graphmat);
-	
-	cv::waitKey(0);
-	
+	process(file, false);
+
 	
 }
